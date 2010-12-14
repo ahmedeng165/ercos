b2c12b4fd9083be0f903e8d5b893d2429ba77f92
aitorvs <aitorvs@05586ece-abd6-0ffb-e60e-8de798876aaa>
2010-12-14 12:50:44 +0000

    - defs modified so SPARCPRO can be compiled
    M    defs.mk

--- branches/sparcpro/defs.mk
+++ branches/sparcpro/defs.mk
@@ -45,6 +45,46 @@ endif # CONFIG_MP_LEON3 == y
 
 endif # CONFIG_ARCH_SPARC == y
 
+#--------------------- FOR SPARCPRO ------------------------
+ifeq ($(CONFIG_ARCH_SPARCPRO), y)
+
+#LIBGCC:=#/opt/rtems-4.10/lib/gcc/sparc-rtems/4.3.3/v8/libgcc.a
+LIBGCC:=#$(shell $(CC) -print-libgcc-file-name)
+
+include $R/os/ercos/hals/sparcpro/defs.mk
+
+ifeq 	($(CONFIG_MP_ERC32), y)
+
+include $R/os/ercos/hals/sparcpro/erc32/defs.mk	
+
+ifeq 		($(CONFIG_BOARD_UNDEFINED), y)
+#nothing here at the moment
+endif #CONFIG_BOARD_UNDEFINED == y
+
+endif # CONFIG_MP_ERC32 == y
+
+ifeq 	($(CONFIG_MP_LEON2), y)
+
+include $R/os/ercos/hals/sparcpro/leon2/defs.mk
+
+ifeq 		($(CONFIG_BOARD_UNDEFINED), y)
+#nothing here at the moment
+endif #CONFIG_BOARD_UNDEFINED == y
+
+endif # CONFIG_MP_LEON2 == y
+
+ifeq 	($(CONFIG_MP_LEON3), y)
+	
+include $R/os/ercos/hals/sparcpro/leon3/defs.mk
+
+ifeq 		($(CONFIG_BOARD_UNDEFINED), y)
+#nothing here at the moment
+endif #CONFIG_BOARD_UNDEFINED == y
+
+endif # CONFIG_MP_LEON3 == y
+
+endif # CONFIG_ARCH_SPARCPRO == y
+
 #--------------------- FOR IA32 ------------------------
 ifeq ($(CONFIG_ARCH_IA32), y)
  
